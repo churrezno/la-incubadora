25d75cfc9586f34373ca806b97724fe2bfcb1904
churrezno <jorge@devala.es>
2026-06-02 13:46:57 +0200

    update logo in admin panel sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -65,7 +65,7 @@ return [
     */
 
     'logo' => '',
-    'logo_img' => 'storage/images/logo_ECAM-industria_La_incubadora.svg',
+    'logo_img' => 'storage/images/logo_La_Incubadora.svg',
     'logo_img_class' => 'logo-ecam',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
